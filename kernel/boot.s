de3790a7f39279e6abbbede0215e9e04b9482780
Grzegorz Kraszewski <krashan@teleinfo.pb.edu.pl>
2018-11-09 22:52:35 +0100

    Something compiles, not tested yet.

---
+++ kernel/boot.s
@@ -0,0 +1,22 @@
+				.section ".text"
+
+				.xdef	_start
+				.xref	main
+
+_start:
+				LDR		r1,=_start
+				MOV		sp,r1
+/*
+				LDR		r1,=__bss_start
+				LDR		r2,=__bss_size
+				CBZ		r2,bss_cleared
+clearing:
+				STR		r0,[r1],#4
+				SUB		r2,r2,#1
+				CBNZ	r2,clearing
+bss_cleared:
+*/
+				BL		main
+infinity:
+				B		infinity
+
