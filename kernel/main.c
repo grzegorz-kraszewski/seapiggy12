3aefe7a9e03dfbd7ebcfb3c3a703d833e83359a8
Grzegorz Kraszewski <krashan@teleinfo.pb.edu.pl>
2018-11-10 18:58:06 +0100

    Added .bss section clearing, so globals will be set to zero.

--- kernel/main.c
+++ kernel/main.c
@@ -1,4 +1,19 @@
+#include <stdint.h>
+
+
+extern uint32_t *__bss_start__, *__bss_end__;
+
+
+static void BssClear(void)
+{
+	uint32_t *bss = __bss_start__;
+
+	while (bss < __bss_end__) *bss++ = 0;
+}
+
+
 void Main(void)
 {
+	BssClear();
 	while (1);
 }
