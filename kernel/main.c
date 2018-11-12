15c3f42d30742233a19457ecbdfb79b323251d81
Michal Schulz <michal.schulz@tu-clausthal.de>
2018-11-12 14:00:40 +0100

    __bss_start and __bss_end are not variables, these are symbols marking beginning and end of .bss section. When clearing .bss take *addresses* of these symbols.

--- kernel/main.c
+++ kernel/main.c
@@ -1,14 +1,14 @@
 #include <stdint.h>
 
 
-extern uint32_t *__bss_start__, *__bss_end__;
+extern uint32_t __bss_start__, __bss_end__;
 
 
 static void BssClear(void)
 {
-	uint32_t *bss = __bss_start__;
+	uint32_t *bss = &__bss_start__;
 
-	while (bss < __bss_end__) *bss++ = 0;
+	while (bss < &__bss_end__) *bss++ = 0;
 }
 
 
