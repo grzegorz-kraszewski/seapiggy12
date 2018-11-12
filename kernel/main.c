5b055442a9e5284a165d1b2d5a50ba9acd5ad313
Michal Schulz <michal.schulz@tu-clausthal.de>
2018-11-12 08:04:31 +0100

    Enable D- and I-caches. Disable strict alignment. Enable branch prediction.

--- kernel/main.c
+++ kernel/main.c
@@ -14,6 +14,16 @@ static void BssClear(void)
 
 void Main(void)
 {
+	uint32_t tmp;
 	BssClear();
+	
+	/* Enable caches and branch prediction. Enable unaligned accesses */
+	asm volatile("mrc p15, 0, %0, c1, c0, 0" : "=r"(tmp));
+	tmp |= (1 << 2) | (1 << 12) | (1 << 11); // D-Cache, I-Cache, Branch prediction, in that order
+	tmp = (tmp | (1 << 22)) & ~(1 << 1);     // Unaligned loads and stores enabled, Strict alignment disabled
+	asm volatile("mcr p15, 0, %0, c1, c0, 0" ::"r"(tmp));
+	
+	
+	
 	while (1);
 }
