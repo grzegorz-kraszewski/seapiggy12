ee7bbf5af16f2081450043d92e075f136a520dd2
Grzegorz Kraszewski <krashan@teleinfo.pb.edu.pl>
2018-11-25 19:22:39 +0100

    Removed/blocked unused code.

--- kernel/main.c
+++ kernel/main.c
@@ -1,10 +1,12 @@
 #include <stdint.h>
 
 #include "video.h"
+#include "debug.h"
 
 
 extern uint32_t __bss_start__, __bss_end__;
 
+#if 0
 
 static void BssClear(void)
 {
@@ -13,29 +15,13 @@ static void BssClear(void)
 	while (bss < &__bss_end__) *bss++ = 0;
 }
 
+#endif
 
 void Main(void)
 {
-	BssClear();
-	
-	/* 
-	   Enable cp10 an cp11 coprocessors. These are VFP Single (cp10) and 
-	   double (cp11) precision
-	*/
+	uint32_t i;
 
-/*
-	asm volatile("mrc p15, 0, %0, c1, c0, 2" : "=r"(tmp));
-	tmp |= (3 << 20) | (3 << 22); // Full access to cp10 and cp11
-	asm volatile("mcr p15, 0, %0, c1, c0, 2" ::"r"(tmp));
-*/
-	/* 
-	    VFP coprocessors enabled. Now set the EN bit of FPEXC register in order
-	    to allow VFP instructions
-	*/
-/*
-	tmp = (1 << 30); // The EN bit
-	asm volatile("fmxr fpexc, %0"::"r"(tmp));
-*/	
-	SetVideoMode(640, 512);	
+	kputs("Main() reached.\r\n");
+
 	while (1);
 }
